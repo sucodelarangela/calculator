6d472f06d9466c77dc96e4e417e8f407f7dee3e8
Angela Caldas <86853033+sucodelarangela@users.noreply.github.com>
2021-12-03 19:52:30 -0300

    Replaced var with let

--- script.js
+++ script.js
@@ -5,64 +5,64 @@ function limpaTela() {
 }
 
 function backspace() {
-  var numero = document.getElementById("visor").innerHTML;
-  var novoNumero = parseFloat(numero.slice(0, -1));
+  let numero = document.getElementById("visor").innerHTML;
+  let novoNumero = parseFloat(numero.slice(0, -1));
   console.log(novoNumero)
   visor.innerHTML = novoNumero;
 }
 
 function divide() {
-  var numero = document.getElementById("visor").innerHTML;
-  var dividendo = parseFloat(numero.slice(0, -3));
+  let numero = document.getElementById("visor").innerHTML;
+  let dividendo = parseFloat(numero.slice(0, -3));
   visor.innerHTML = "";
   parcial.innerHTML = dividendo + " ÷ ";
 }
 
 function multiplica() {
-  var numero = document.getElementById("visor").innerHTML;
-  var fatorUm = parseFloat(numero.slice(0, -3));
+  let numero = document.getElementById("visor").innerHTML;
+  let fatorUm = parseFloat(numero.slice(0, -3));
   visor.innerHTML = "";
   parcial.innerHTML = fatorUm + " x ";
 }
 
 function soma() {
-  var numero = document.getElementById("visor").innerHTML;
-  var primeiroNumero = parseFloat(numero.slice(0, -3));
+  let numero = document.getElementById("visor").innerHTML;
+  let primeiroNumero = parseFloat(numero.slice(0, -3));
   visor.innerHTML = "";
   parcial.innerHTML = primeiroNumero + " + ";
 }
 
 function subtrai() {
-  var numero = document.getElementById("visor").innerHTML;
-  var primeiroNumero = parseFloat(numero.slice(0, -3));
+  let numero = document.getElementById("visor").innerHTML;
+  let primeiroNumero = parseFloat(numero.slice(0, -3));
   visor.innerHTML = "";
   parcial.innerHTML = primeiroNumero + " - ";
 }
 
 function mostraResultado() {
-  var operador = document.getElementById("parcial").innerHTML;
+  let operador = document.getElementById("parcial").innerHTML;
   if (operador.includes(" ÷ ") == true) {
-    var dividendo = parseFloat(operador.slice(0, -3));
-    var divisor = parseFloat(document.getElementById("visor").innerHTML);
-    var operacao = dividendo / divisor;
+    let dividendo = parseFloat(operador.slice(0, -3));
+    let divisor = parseFloat(document.getElementById("visor").innerHTML);
+    let operacao = dividendo / divisor;
     visor.innerHTML = operacao;
     parcial.innerHTML = dividendo + " ÷ " + divisor + " = ";
   } else if (operador.includes(" x ") == true) {
-    var fatorUm = parseFloat(operador.slice(0, -3));
-    var fatorDois = parseFloat(document.getElementById("visor").innerHTML);
-    var operacao = fatorUm * fatorDois;
+    let fatorUm = parseFloat(operador.slice(0, -3));
+    let fatorDois = parseFloat(document.getElementById("visor").innerHTML);
+    let operacao = fatorUm * fatorDois;
     visor.innerHTML = operacao;
     parcial.innerHTML = fatorUm + " x " + fatorDois + " = ";
   } else if (operador.includes(" + ") == true) {
-    var primeiroNumero = parseFloat(operador.slice(0, -3));
-    var segundoNumero = parseFloat(document.getElementById("visor").innerHTML);
-    var operacao = primeiroNumero + segundoNumero;
+    let primeiroNumero = parseFloat(operador.slice(0, -3));
+    let segundoNumero = parseFloat(document.getElementById("visor").innerHTML);
+    let operacao = primeiroNumero + segundoNumero;
     visor.innerHTML = operacao;
     parcial.innerHTML = primeiroNumero + " + " + segundoNumero + " = ";
   } else if (operador.includes(" - ") == true) {
-    var primeiroNumero = parseFloat(operador.slice(0, -3));
-    var segundoNumero = parseFloat(document.getElementById("visor").innerHTML);
-    var operacao = primeiroNumero - segundoNumero;
+    let primeiroNumero = parseFloat(operador.slice(0, -3));
+    let segundoNumero = parseFloat(document.getElementById("visor").innerHTML);
+    let operacao = primeiroNumero - segundoNumero;
     visor.innerHTML = operacao;
     parcial.innerHTML = primeiroNumero + " - " + segundoNumero + " = ";
   }
